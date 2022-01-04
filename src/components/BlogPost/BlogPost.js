bbf6c2a8bc7eb18d2eae99d780752db1b38fde9b
caique <caique_sarkis_corebiz@whirlpool.com>
2022-01-04 01:47:53 -0300

    Math

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -1,24 +1,11 @@
 import { gql, useQuery } from '@apollo/client';
-import {useParams, Link} from "react-router-dom"
+import {useParams} from "react-router-dom"
 import ReactMarkdown from 'react-markdown'
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import {vscDarkPlus} from 'react-syntax-highlighter/dist/esm/styles/prism'
-import RemarkMathPlugin from 'remark-math';
-import rehypeRaw from 'rehype-raw'
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 import './BlogPost.css'
 
-const _mapProps = (props) => ({
-  ...props,
-  escapeHtml: false,
-  plugins: [
-    RemarkMathPlugin
-  ],
-  renderers: {
-    ...props.renderers,
-    math: ({ value }) => `math: ${value}`,
-    inlineMath: ({ value }) => `inlineMath: ${value}`
-  }
-}); 
 
 function BlogPost() {
     const GET_POSTS = gql`
@@ -42,31 +29,41 @@ function BlogPost() {
     return (
       <div className='blog-post-container'>
         <div className='BlogPost'>
-            <h1>{posts[postId].Title}</h1>            
-            <ReactMarkdown 
-            className='remark'
-            children={posts[postId].Body}
-            rehypePlugins={[rehypeRaw]}
-            components={{
-              code({node, inline, className, children, ...props}) {
-                const match = /language-(\w+)/.exec(className || '')
-                return !inline && match ? (
-                  <SyntaxHighlighter
-                    children={String(children).replace(/\n$/, '')}
-                    style={vscDarkPlus}
-                    language={match[1].toLocaleLowerCase()}
-                    PreTag="div"
-                    {..._mapProps(props)}
-                  />
-                ) : (
-                  <code className={className} {..._mapProps(props)}>
-                    {children}
-                  </code>
-                )
-              }
-            }}>
-            </ReactMarkdown>
+            <h1>{posts[postId].Title}</h1>   
             
+                <ReactMarkdown 
+                className='remark'
+                children={posts[postId].Body}
+                components={{
+                  p({node, inline, className, children, ...props}) {
+                    return(
+                    <MathJaxContext>
+                      <p>
+                        <MathJax> 
+                          {children}
+                        </MathJax>
+                      </p> 
+                    </MathJaxContext>)
+                },
+                  code({node, inline, className, children, ...props}) {
+                    const match = /language-(\w+)/.exec(className || '')
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        children={String(children).replace(/\n$/, '')}
+                        style={vscDarkPlus}
+                        language={match[1].toLocaleLowerCase()}
+                        PreTag="div"
+                        {...props}
+                      />
+                    ) : (
+                      <code className={className} {...props}>
+                        {children}
+                      </code>
+                    )
+                  }
+                }}>
+                </ReactMarkdown>
+              
 
         </div>
       </div>
